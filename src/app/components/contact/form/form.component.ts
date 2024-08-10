73d00bbf681e3067279a0de6dcafe652ac1c84b7
Victor Blanco <12vblanco@gmail.com>
2024-08-10 14:43:16 +0100

    redirect

--- src/app/components/contact/form/form.component.ts
+++ src/app/components/contact/form/form.component.ts
@@ -13,15 +13,21 @@ export class FormComponent {
   onSubmitHandler(event: Event): void {
     event.preventDefault();
     const formElement = event.target as HTMLFormElement;
-  
-    // Option 1: Directly let Netlify handle it, then navigate on success
+    const formData = new FormData(formElement);
+    const formObject: { [key: string]: string } = {};
+    formData.forEach((value, key) => {
+      formObject[key] = value.toString();
+    });
+    console.log('Form Data:', formObject);
+
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams(new FormData(formElement) as any).toString(),
+      body: new URLSearchParams(formObject).toString(),
     })
       .then((response) => {
         if (response.ok) {
+          console.log('Form submission successful');
           this.router.navigate(['/success']);
         } else {
           throw new Error('Form submission failed');
